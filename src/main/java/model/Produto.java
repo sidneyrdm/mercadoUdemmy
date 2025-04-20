69ccdbdf29d2950b172cb3499fd111368c664ba7
sidneyrdm <sidneybritomal@gmail.com>
2025-04-20 10:42:35 -0300

    criação da classe de produto.

--- src/main/java/model/Produto.java
+++ src/main/java/model/Produto.java
@@ -1,4 +1,49 @@
 package model;
 
+import helper.Utils;
+
 public class Produto {
+
+    private static int contador = 1;
+
+    private int codigo;
+    private String nome;
+    private Double preco;
+
+
+    public Produto(String nome, Double preco) {
+        this.codigo = Produto.contador;
+        this.nome = nome;
+        this.preco = preco;
+        Produto.contador += 1;
+    }
+
+    public int getCodigo() {
+        return this.codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "codigo=" + this.getCodigo() +
+                ", nome='" + this.getNome() + '\'' +
+                ", Preco=" + Utils.doubleParaString(this.getPreco()) +
+                '}';
+    }
 }
